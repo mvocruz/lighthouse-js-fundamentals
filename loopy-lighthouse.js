1e591052f4dff9428b96258601b323e95698425b
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2020-10-13 09:36:30 +0000

    Loops training Lighthouse

---
+++ loopy-lighthouse.js
@@ -0,0 +1,12 @@
+let num = 100
+for (num >= 100; num <= 200; num++) {
+  if ((num % 3 === 0) && (num % 4 !== 0)){
+  console.log('Loopy');
+} else if ((num % 3 !== 0) && (num % 4 === 0)){
+  console.log('Lighthouse');
+} else if ((num % 3 === 0) && (num % 4 === 0)) {
+  console.log('LoopyLighthouse')
+} else {
+  console.log(num);
+}
+}
